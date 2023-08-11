7f8564089a4094ceba087e5eaf80e6d1bce79d59
abdelhalim lakfifi <abdelhalimlakfifi@gmail.com>
2023-08-11 11:26:00 +0100

    make the open source project

---
+++ assets/js/button.js
@@ -0,0 +1,11 @@
+document.getElementById('openSource').addEventListener('mouseover',function () {
+    document.getElementById('openSourceSvg').style.fill='black';
+    // console.log('hover');
+})
+
+
+document.getElementById('openSource').addEventListener('mouseout',function () {
+    document.getElementById('openSourceSvg').style.fill='white';
+    // console.log('ssss');
+
+})
